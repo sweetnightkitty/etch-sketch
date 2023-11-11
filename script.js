0d135098b9aa1b9c63b2622ab907724242bb5865
SaraToth <saratoth101@gmail.com>
2023-11-11 18:29:06 +0900

    Create one function to populate the units in a row, and create function to populate all rows. Only first function works properly. Add styles to temporarily view divs live

---
+++ script.js
@@ -0,0 +1,27 @@
+const dimensions = 16;
+const container = document.querySelector(".container");
+
+
+function makeRow() {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (i = 0; i < dimensions; i++) {
+        const unit = document.createElement("div");
+        unit.classList.add("unit");
+
+        row.appendChild(unit);
+    }
+
+    return row;
+}
+
+function appendRow() {
+
+    for (i = 0; i < dimensions; i++) {
+        const row = makeRow();
+        container.appendChild(row);
+    }
+}
+
+appendRow();
