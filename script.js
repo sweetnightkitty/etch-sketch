7e54c893c466138996fb9f5cf060479d26f9e8a0
SaraToth <saratoth101@gmail.com>
2023-11-12 19:10:50 +0900

    Change mouseover event listener color change from pink to orange

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const units = document.querySelectorAll(".unit");
 
 units.forEach(function(i) {
     i.addEventListener('mouseover', () => {
-        i.style.backgroundColor = "pink";
+        i.style.backgroundColor = "orange";
     })
 })
 
